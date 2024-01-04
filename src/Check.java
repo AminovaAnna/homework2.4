853f26a10d64ab5b81f058638507432520285141
AminovaAnna <aminovaanna163@gmail.com>
2024-01-04 21:14:38 +0300

    проверьте пожалуйста домашнее задание

--- src/Check.java
+++ src/Check.java
@@ -1,9 +1,25 @@
 public class Check {
-    public static void checkUser(String login){
-     if (login.length() > 20 || login.contains(".") || login.contains("-")) {
-         throw new IllegalArgumentException("Недопустимые переменные");
-     }
+    public static final String REGEX = "^[a-zA-Z0-9_]*$";
+
+    public static void checkUser(User user) {
+
+        if (!user.getLogin().matches(REGEX)) {
+
+            throw new WrongLoginException("В логине есть недопустимые символы");
+        }
+        if (user.getLogin().length()>20){
+            throw new WrongLoginException("Недопустимая длина логина");
+        }
+        if (!user.getPassword().equals(user.getConfirmPassword())) {
+            throw new WrongPasswordException("Пароли не совпадают");
+        }
+        if (!user.getPassword().matches(REGEX)) {
+            throw new WrongPasswordException("В пароле есть недопустимые символы");
+        }
+        if (user.getPassword().length()>20){
+            throw new WrongPasswordException("Недопустимая длина пароля");
+        }
     }
-
-
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-User user1 = new User(
-        "java_skypro_go",
-        "D_1hWiKjjP_9",
-        "D_1hWiKjjP_9");
-User user2 = new User(
-        "Anna_123456",
-        "A_123_321_55",
-        "A_123_321-55");
-User user3 = new User(
-        "mini_pig",
-        "potato_668855",
-        "potato_565656");
-User user4 = new User(
-        "forgetmyloginandpassword",
-        "asd123",
-        "asd123");
+        User user1 = new User(
+                "java_skypro_go",
+                "D_1hWiKjjP_9",
+                "D_1hWiKjjP_9");
+        User user2 = new User(
+                "Anna_123456",
+                "A_123_321_55",
+                "A_123_321-55");
+        User user3 = new User(
+                "forgetmyloginandpassword",
+                "asd123",
+                "asd123");
 
-user2.getLogin().checkUser
+
+        Check.checkUser(user1);
+        Check.checkUser(user2);
+        Check.checkUser(user3);
 
     }
 
